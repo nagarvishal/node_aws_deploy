f19e43245ccfb8d418d8cda91129bb0509c817e7
Vishal Nagar <vishalnagar@Vishals-MacBook-Air.local>
2025-10-31 02:08:35 +0530

    My Changes

--- index.js
+++ index.js
@@ -8,6 +8,13 @@ app.get("/api/get", (req,resp)=>{
         message : "Nodejs AWS Deployment"
     })
 })
+app.get("/api/user", (req,resp)=>{
+    resp.send({
+        username: "Vishalnagar",
+        password : "kushal@2002",
+        address : "Madhoarjapuar, Kehsortiapatan, kota, rajasthan"
+    })
+})
 
 app.listen(9000,(req,res)=>{
     console.log("server running on port 9000");
